076e3f6a9ab656bf60d12d84d10bd51a882ce28b
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-09-12 10:50:42 +0700

    Menambahkan persentase dalam semua nilai

--- Siakad20.java
+++ Siakad20.java
@@ -5,7 +5,7 @@ public class Siakad20 {
     String nama, nim;
     char kelas;
     byte absen;
-    double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+    double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS ,nilaiAkhir;
 
     System.out.print("Masukkan nama: ");
     nama = sc.nextLine();
@@ -19,9 +19,12 @@ public class Siakad20 {
     nilaiKuis = sc.nextDouble();
     System.out.print("Masukkan nilai tugas: ");
     nilaiTugas = sc.nextDouble();
-    System.out.print("Masukkan nilai ujian: ");
-    nilaiUjian = sc.nextDouble();
-    nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+    System.out.print("Masukkan nilai UTS: ");
+    nilaiUTS = sc.nextDouble();
+    System.out.print("Masukkan nilai UAS: ");
+    nilaiUAS = sc.nextDouble();
+    nilaiAkhir = ((nilaiKuis * 0.20 ) + (nilaiTugas * 0.15) + (nilaiUTS * 0.30) + (nilaiUAS * 0.35)) / 3;
+    
     System.out.println("Nama: " + nama + "NIM:  " + nim);
     System.out.println("Kelas: " + kelas + "Absen: " + absen);
     System.out.println("Nilai Akhir: " + nilaiAkhir );
