a2e93f4cf867e165f4df659fc557ad09c3fa02f9
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-09-12 12:31:04 +0700

    Mengerjakan tugas pertama

---
+++ Tugas01Jobsheet3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Tugas01Jobsheet3{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double tarifLis = 1500;
+        System.out.print("Jumlah penggunaan listrik: ");
+        double penggunaan = input.nextDouble();
+
+        double totalTagihan = tarifLis * penggunaan;
+        boolean banyakpenggunaan = penggunaan > 500;
+        System.out.println("total tagihan Listrik: Rp " + totalTagihan);
+        System.out.println(" melebihi 500 atau tidak = " + banyakpenggunaan);
+
+
+
+
+    }
+}
+
