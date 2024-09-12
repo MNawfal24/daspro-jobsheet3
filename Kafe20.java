645c179a1d408443bb12de5501b919248bbf2f77
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-09-12 11:05:22 +0700

    casting narrowing double ke int dalam "nominalBayar"

--- Kafe20.java
+++ Kafe20.java
@@ -3,7 +3,7 @@ public class Kafe20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean keanggotaan;
-        int jmlKopi, jmlTeh, jmlRoti;
+        int jmlKopi, jmlTeh, jmlRoti, nominalInt;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
         float diskon = 10 / 100f;
 
@@ -18,13 +18,11 @@ public class Kafe20 {
 
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
         nominalBayar = totalHarga - (diskon * totalHarga);
+        nominalInt = (int) nominalBayar;
 
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + "kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " + nominalBayar);
-
-
+        System.out.println ("nominal bayar setleah casting Rp " + nominalInt);
     }
 }
-
-
